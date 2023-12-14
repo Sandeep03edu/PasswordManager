836ba79b054e3f2b20b86d2e595c0def52ec317e
Sandeep Mishra <sandeep03edu@gmail.com>
2023-12-15 01:11:28 +0530

    Added UserEmailExist Api

--- Backend/controller/userAuthController.js
+++ Backend/controller/userAuthController.js
@@ -142,4 +142,31 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { registerUser, loginUser };
+const userEmailExist = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+
+  // Handling Errors
+  if (!email) {
+    return res.status(400).json({
+      success: false,
+      error: "Email id missing!!",
+    });
+  }
+
+  try {
+    // Check whether user exist or not
+    const userExist = await User.findOne({ email });
+
+    return res.status(201).json({
+      success: true,
+      userExist: userExist !== null && userExist !== undefined,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      error: "An internal Error occurred!! ",
+    });
+  }
+});
+
+module.exports = { registerUser, loginUser, userEmailExist };
--- Backend/routes/userAuthRoutes.js
+++ Backend/routes/userAuthRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { loginUser, registerUser } = require("../controller/userAuthController");
+const {
+  loginUser,
+  registerUser,
+  userEmailExist,
+} = require("../controller/userAuthController");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 
 router.post("/login", loginUser);
+
+router.post("/emailExist", userEmailExist);
 
 module.exports = router;
