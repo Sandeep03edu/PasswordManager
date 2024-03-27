5e92fb8e0a4a3cf2f17a86df5a19150fb746ad1d
Sandeep Mishra <sandeep03edu@gmail.com>
2024-03-27 16:53:19 +0530

    Added log messages in chatbot.py

--- Backend/controller/chatbot.py
+++ Backend/controller/chatbot.py
@@ -1,4 +1,5 @@
 try:
+    print("Started!!")
     import os
     import sys
     import json
@@ -10,19 +11,28 @@ try:
     from langchain_community.llms.huggingface_endpoint import HuggingFaceEndpoint
     import warnings
 
+    print("Import Successfull!!")
+
     # Disable all warnings
     warnings.filterwarnings("ignore")
 
     # Read queries from standard input and print responses to standard output
     inp = sys.stdin
+
+    print("Input", inp)
     for line in sys.stdin:
+        print("Line", line.strip())
         if '#080819082709#' in line.strip():
             tokens = line.split('#080819082709#')
+
+            print("Has Tokens:: " , tokens)
 
             # Load the text document
             loader = TextLoader("controller//data//app.txt")
             document = loader.load()
             os.environ["HUGGINGFACEHUB_API_TOKEN"] = tokens[1].strip()
+
+            print("Token:: " , tokens[1].strip())
 
             # Split the document into chunks
             text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
@@ -35,8 +45,11 @@ try:
             # Load the Question-Answering chain
             llm = HuggingFaceEndpoint(repo_id="google/flan-t5-xxl", temperature=0.8, model_kwargs={"max_length": 512})
             chain = load_qa_chain(llm, chain_type="stuff")
+
+            print("Setup Completed!!")
         else:
             query = line.strip()
+            print("Has query:: ", query)
 
             # Perform similarity search
             querySimilarDocs = db.similarity_search(query)
@@ -45,8 +58,10 @@ try:
             res = chain.run(input_documents=querySimilarDocs, question=query)
 
             # Print the response to standard output
+            print("Result", res)
             print(json.dumps( "##080819082709##"+ res))
             sys.stdout.flush()  # Flush the output buffer to ensure response is sent immediately
 except Exception as err:
+    print("Errror", err)
     print(json.dumps( "Error:: " + err))
     sys.stdout.flush()  # Flush the output buffer to ensure response is sent immediately
