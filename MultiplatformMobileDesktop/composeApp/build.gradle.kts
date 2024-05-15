345134f7b0226feeeee8f0beccacd90ffcd1ea74
Sandeep Mishra <sandeep03edu@gmail.com>
2024-05-16 01:10:05 +0530

    Android Production version 1.04 (Version code = 6) published

--- MultiplatformMobileDesktop/composeApp/build.gradle.kts
+++ MultiplatformMobileDesktop/composeApp/build.gradle.kts
@@ -161,8 +161,8 @@ kotlin {
     }
 }
 
-val releasePackageVersion = "1.03"
-val releaseDesktopPackageVersion = "1.0.3"
+val releasePackageVersion = "1.04"
+val releaseDesktopPackageVersion = "1.0.4"
 
 android {
     namespace = "com.sandeep03edu.passwordmanager"
@@ -176,7 +176,7 @@ android {
         applicationId = "com.sandeep03edu.passwordmanager.android"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 5
+        versionCode = 6
         versionName = releasePackageVersion
     }
     buildFeatures {
